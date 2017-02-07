88ef666db5b2a86baf0e9b5ac4f3c54e7cfa361c
kangoula <denisgme@gmail.com>
2017-02-07 11:17:14 +0100

    rename webapp to public and serve static files through express

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();
-var db = mongoose.connect('mongodb://localhost/wally');
-var Schema = mongoose.Schema;
+var db = mongoose.connect('mongodb://localhost/Wally');
 
+// MONGO config for the project
+var Schema = mongoose.Schema;
 // maping for the stock object in mongo
 var StockSchema = new Schema({
   name: String,
@@ -13,12 +15,33 @@ var StockSchema = new Schema({
 });
 var Stock = mongoose.model('Stock', StockSchema);
 
+app.use(express.static(__dirname + '/public'));
+app.use('/node_modules',  express.static(__dirname + '/node_modules'));
+app.use(bodyParser.json());
+
 app.get('/', function(req, res){
   res.send('Hello World');
 });
 
-app.get('/stocks', function(req, res, next){
-
+app.route('/stocks')
+.get(function(req, res, next){
+  Stock.find({}, function(err, stocks){
+    if(err){
+      return next(err);
+    } else {
+      res.json(stocks);
+    }
+  })
+})
+.post(function(req, res, next){
+    var stock = new Stock(req.body);
+    stock.save(function(err){
+      if(err){
+        return next(err);
+      } else {
+        return res.json(stock);
+      }  
+  });
 });
 
 app.listen('3000');
