9962e0be8f5ea5d540fe42880155259c55eff133
kangoula <denisgme@gmail.com>
2017-02-07 12:28:31 +0100

    add call to remote stock service

--- public/app/search/search.controller.js
+++ public/app/search/search.controller.js
@@ -5,16 +5,24 @@
         .module('wally.search')
         .controller('Search', Search);
 
-    Search.$inject = ['$scope']
+    Search.$inject = ['$scope', '$http']
 
-    function Search($scope) {
+    function Search($scope, $http) {
         var vm = this;
-        vm.stocks = [];
+        vm.data = [];
         vm.title = 'Search';
 
-        $scope.name = "test";
+        $scope.name = "search";
 
-        console.log('ok');
-
+        // search stocks from ws
+        var base_url = 'https://query.yahooapis.com/v1/public/yql?q=';
+        var data = encodeURIComponent('select * from yahoo.finance.quotes where symbol = "YHOO"');
+        var url2 = base_url + data + "&env=http%3A%2F%2Fdatatables.org%2Falltables.env&format=json";
+        
+        $http.get(url2)
+            .then(function(response) {
+                console.log(response);
+            vm.data = response.data;
+        });
     }
 })();
