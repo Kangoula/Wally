49b59282b9eadeba7e0816a4735334aebbfe5425
kangoula <denisgme@gmail.com>
2017-02-11 18:16:23 +0100

    send request to yahoo finance to get stock data

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var request = require('request');
 var bodyParser = require('body-parser');
 
 var app = express();
@@ -42,6 +43,24 @@ app.route('/api/stocks')
   });
 });
 
+app.route('/api/search/:symbol')
+.get(function(req, res, next){
+  var symbol = req.params.symbol;
+  
+  var query = encodeURIComponent('select * from yahoo.finance.quotes where symbol = "' + symbol + '"');
+  var url = "https://query.yahooapis.com/v1/public/yql?q=env%20'store%3A%2F%2Fdatatables.org%2Falltableswithkeys'%3B%20"+query+"&format=json"
+
+  request(url, function(error, response, body ){
+    if(!error && response.statusCode === 200){
+      return res.send(body);
+    }
+    else
+      return next(error);
+  });
+
+  
+});
+ 
+
 app.listen('3000');
-console.log('Server is running...');
 
