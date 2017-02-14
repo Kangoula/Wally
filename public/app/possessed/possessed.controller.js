9c98ae329f02734238cadddf2b61faf623e9ecdf
kangoula <denisgme@gmail.com>
2017-02-14 19:40:09 +0100

    Handle change in stock list when a stock is sold

--- public/app/possessed/possessed.controller.js
+++ public/app/possessed/possessed.controller.js
@@ -5,30 +5,55 @@
         .module('wally')
         .controller('MyStocks', MyStocks);
 
-    MyStocks.$inject = ['$scope', 'StocksService', '$rootScope'];
+    MyStocks.$inject = ['$scope', 'Stock', '$rootScope', 'Search'];
 
-    function MyStocks($scope, StocksService, $rootScope) {
+    function MyStocks($scope, Stock, $rootScope, Search) {
         var vm = this;
+        vm.all = [];
+        vm.getCurrentPrice = getCurrentPrice;
 
         $scope.stocks = [];
         $scope.loading = true;
         $scope.search = false;
-        $scope.mystocks = StocksService;
-        $scope.$on('stock.bought', updateStoks);
+        $scope.mystocks = Stock;
+        $scope.$on('stock.updated', updateStoks);
 
-        StocksService.query({}, onSuccess, onError);
+        Stock.query({}, onSuccess, onError);
+        //updateStoks();
 
         function onSuccess(response) {
-            $scope.loading = false;
-            $scope.stocks = response;
+
+            if (response.length > 0) {
+                // get current price for each Stock
+                response.forEach(function (value) {
+                    vm.getCurrentPrice(value)
+                });
+                $scope.loading = false;
+                $scope.stocks = vm.all;
+            } else {
+                $scope.loading = false;
+                $scope.stocks = [];
+            }
+
         }
 
         function onError() {
             $scope.loading = false;
         }
 
-        function updateStoks(event, value) {
-            $scope.stocks.unshift(value);
+        function updateStoks() {
+            console.log("update stocks");
+            $scope.loading = true;
+            Stock.query({}, onSuccess, onError);
+        }
+
+        function getCurrentPrice(value) {
+            Search.get({
+                symbol: value.symbol
+            }, function (res) {
+                value.currentPrice = res.query.results.quote.Ask;
+                vm.all.push(value);
+            });
         }
     }
 })();
