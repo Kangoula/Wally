ae4256b3bba849e0a95f21642daac16e633e6618
kangoula <denisgme@gmail.com>
2017-02-14 19:37:22 +0100

    add wallet stats

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var request = require('request');
 var bodyParser = require('body-parser');
 
 var app = express();
+var router = express.Router();
+
 var db = mongoose.connect('mongodb://localhost/Wally');
 
 // MONGO config for the project
@@ -15,57 +17,186 @@ var StockSchema = new Schema({
   price: Number,
   dateAdded: Date
 });
+var WalletSchema = new Schema({
+  date: Date,
+  total: Number
+});
 var Stock = mongoose.model('Stock', StockSchema);
+var Wallet = mongoose.model('Wallet', WalletSchema);
 
 // serve static files
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
+
 // parse body of request in json
 app.use(bodyParser.json());
+app.use('/api', router);
 
-app.route('/api/stocks')
-  .get(function (req, res, next) {
-    // get all stocks ordered by date
-    Stock.find({}).sort({
-      dateAdded: 'desc'
-    }).exec(function (err, stocks) {
-      if (err) {
-        return next(err);
-      } else {
-        res.json(stocks);
-      }
-    })
+
+// STOCKS 
+router.route('/stocks')
+  // get all stocks 
+  .get((req, res, next) => {
+    console.log("[GET] /stocks ");
+    // group stocks buy name and price
+    Stock.aggregate([{
+          "$sort": {
+            'symbol': -1,
+            'price': 1
+          }
+        },
+        {
+          "$group": {
+            _id: {
+              "name": "$name",
+              "price": "$price",
+            },
+            result: {
+              "$last": {
+                id: "$_id",
+                symbol: "$symbol"
+              }
+            },
+            count: {
+              $sum: 1
+            }
+          }
+        }
+      ])
+      .exec((err, array) => {
+        if (!err) {
+          var stocks = array.map((val) => {
+            
+            return {
+              "_id": val.result.id,
+              "name": val._id.name,
+              "symbol": val.result.symbol,
+              "price": val._id.price,
+              "count": val.count,
+            }
+          });
+          return res.json(stocks);
+        } else {
+          return next(err);
+        }
+      });
   })
-  .post(function (req, res, next) {
-
+  // create new stock
+  .post((req, res, next) => {
+    console.log("[POST] /stocks");
     // create a stock with the values in request body
-    var s = {
+    var stock = new Stock({
       symbol: req.body.symbol.toUpperCase(),
       price: req.body.price,
       name: req.body.name,
-      dateAdded: new Date(req.body.dateAdded)
-    };
+      dateAdded: new Date()
+    });
 
-    var stock = new Stock(s);
-
-    stock.save(function (err) {
+    stock.save((err) => {
       if (err) {
         return next(err);
       } else {
+        console.log("Created new stock with id : " + stock._id);
         return res.json(stock);
+      }
+    });
+  })
+  // delete stock
+  .delete((req, res, next) => {
+    console.log("[DELETE] /stocks")
+    Stock.remove({
+      id: req.param.id
+    }).exec((err) => {
+      if (err) {
+        return next(err);
+      } else {
+        return res.send(204);
       }
     });
   });
 
-app.route('/api/search/:symbol')
-  .get(function (req, res, next) {
+// WALLET
+router.route('/wallet')
+  // add a new entry
+  .post((req, res, next) => {
+    console.log("[POST] /wallet ");
+    // get date, operation type and price of the stock in request body
+    var {
+      price,
+      type,
+      date
+    } = req.body;
+    // get latest wallet value
+    Wallet.find()
+      .limit(1)
+      .sort({
+        date: -1
+      })
+      .exec((err, response) => {
+        if (!err) {
+          if (response.length > 0) {
+            total = response[0].total;
+            // compute total
+            total = type === '-' ? (new Number(total) - new Number(price)) : (new Number(total) + new Number(price));
+          } else {
+            total = price;
+          }
+          // save wallet
+          var wallet = new Wallet({
+            date: new Date(),
+            total: total
+          });
+
+          wallet.save((err) => {
+            if (err) {
+              return next(err);
+            } else {
+              console.log("new wallet entry with value : " + wallet.total);
+              return res.json(wallet);
+            }
+          });
+        }
+      });
+  })
+  // get all wallet values for stat purposes
+  .get((req, res, next) => {
+    console.log("[GET] /wallet ");
+    Wallet.find()
+      .sort({
+        date: -1
+      })
+      .exec((err, values) => {
+        if (!err) {
+          var x = [];
+          var y = [];
+          var total = 0;
+          // compute wallet total amount
+          values.forEach((elem) => {
+            x.push(elem.date);
+            y.push(elem.total);
+          });
+          return res.json({
+            x: x,
+            y: y
+          });
+          
+        } else {
+          return next(err);
+        }
+      });
+  });
+
+// SEARCH
+router.route('/search/:symbol')
+  .get((req, res, next) => {
     var symbol = req.params.symbol;
+    console.log("[GET] /search/"+symbol);
 
     var query = encodeURIComponent('select * from yahoo.finance.quotes where symbol = "' + symbol + '"');
     var url = "https://query.yahooapis.com/v1/public/yql?q=env%20'store%3A%2F%2Fdatatables.org%2Falltableswithkeys'%3B%20" + query + "&format=json"
 
     // search yahoo finance for the symbol
-    request(url, function (error, response, body) {
+    request(url, (error, response, body) => {
       if (!error && response.statusCode === 200) {
         var b = JSON.parse(body);
 
@@ -73,48 +204,21 @@ app.route('/api/search/:symbol')
           return res.send(body);
         }
       }
-      res.sendStatus(404);
     });
   });
 
-// get stats for the wallet amount
-app.route('/api/stats/wallet')
-  .get(function (req, res, next) {
-    // get all stocks ordered by date, from the last to the most recent
-    Stock.find({}).sort({
-      dateAdded: 'asc'
-    }).exec(function (err, stocks) {
-      if (err) {
-        return next(err);
-      } else {
-        var x = [];
-        var y = [];
-        var total = 0;
-        // compute wallet total amount
-        stocks.forEach(function (elem) {
-          x.push(elem.dateAdded);
-          total += elem.price;
-          y.push(total);
-        });
-        return res.json({
-          x: x,
-          y: y
-        });
-      }
-    });
-  });
-
-// get stats for the calendar view
-app.route('/api/stats/calendar')
-  .get(function (req, res, next) {
+// STATS
+router.route('/stats/calendar')
+  .get((req, res, next) => {
+    console.log("[GET] /stats/calendar");
     var map = new Map();
 
-    Stock.find({}).sort({
+    Stock.find().sort({
       dateAdded: 'asc'
-    }).exec(function (err, stocks) {
+    }).exec((err, stocks) => {
       if (!err) {
         // handle sorting and counting with a Map
-        stocks.forEach(function (elem) {
+        stocks.forEach((elem) => {
           var d = new Date(elem.dateAdded);
           var parsed = d.getFullYear() + '/' + parseMonth(d.getMonth()) + '/' + d.getDate();
 
@@ -127,7 +231,7 @@ app.route('/api/stats/calendar')
 
         // array of object to return
         var array = [];
-        map.forEach(function (v, k) {
+        map.forEach((v, k) => {
           array.push({
             date: k,
             value: v
@@ -150,3 +254,6 @@ function parseMonth(month) {
 }
 
 app.listen('3000');
+console.log("Server started :-)");
+
+//
